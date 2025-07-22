dc23833eafdbf05f2faa5bb5562071acc6373217
kalash <kalashsanoniya1818@gmail.com>
2025-07-22 20:57:43 +0530

    git some new commands

--- basic.sh
+++ basic.sh
@@ -1,6 +1,4 @@
 mkdir devops // create a directory
-# Usage: mkdir [options] directory_name
-# Options:
-# -p: Create parent directories as needed
-# -v: Verbose output, showing what is being created
-# Example: mkdir -p /path/to/directory  
+cd ..
+vim hello
+cd devops 
